33976968dd3e068750e36424161ceb116c2c2400
kangod <kangod.yan@gmail.com>
2023-09-20 18:50:02 +0800

    feat: 1、初始化项目：使用eslint、commitlint规范代码和提交信息； 2、渲染静态页面：渲染模板页面、匹配路由、修改Header

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
       "type-enum": [2, "always", ["feat", "fix", "revert"]],
-      "subject-max-length": [1, "always", 30],
+      "subject-max-length": [1, "always", 100],
     },
   };
